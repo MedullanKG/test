783208f4f59705c892695b8edeb8822a01498206
Dean Clarke <deanclarke_811@hotmail.com>
2012-11-08 15:30:01 -0400

    update for my login story

--- WebContent/js/views/templates/login.js
+++ WebContent/js/views/templates/login.js
@@ -1,5 +1,5 @@
 if (!$.isPlainObject(window.JST))
 	window.JST = {};
 
-window.JST["login"] = "<div>Login Controls goes here</div>";
+window.JST["login"] = "<div><form><fieldset><legend>CarePass Log</legend><label>User Name:</label><input type='text' placeholder='Enter username…'><label>Password:</label><input type='text' placeholder='Enter password…'><label class='checkbox'><input type='checkbox'> Check me out</label><button type='submit' class='btn'>Submit</button></fieldset></form>e</div>";
 
